596d54cf6108c3e4d521b540555ba1c342bfb863
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-11-27 14:09:06 +0900

    Rename: outputter to stream_adapter

--- core/build_cpp.sh
+++ core/build_cpp.sh
@@ -11,5 +11,5 @@ OUTPUT_PATH=$3
 shift 3
 
 # ./expand_cpp.sh "$LIBRARY_PATH" "$TARGET" "$EXPANDED_SOURCE_PATH"
-# ccache g++ -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/outputter" -o "$OUTPUT_PATH" "$@" "$EXPANDED_SOURCE_PATH"
-ccache g++ -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/outputter" -o "$OUTPUT_PATH" "$@" "$TARGET"
+# ccache g++ -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/stream_adapter" -o "$OUTPUT_PATH" "$@" "$EXPANDED_SOURCE_PATH"
+ccache g++ -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/stream_adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
