1e3d4e21ca6d89a1046363ef8ebc03134a2f4dc2
uni_kakurenbo <kohma1116@ybb.ne.jp>
2022-12-29 20:41:18 +0900

    Modify: builder (no-misleading-indentation)

--- core/build_cpp.sh
+++ core/build_cpp.sh
@@ -12,4 +12,4 @@ shift 3
 
 # ./expand_cpp.sh "$LIBRARY_PATH" "$TARGET" "$EXPANDED_SOURCE_PATH"
 # ccache g++ -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/stream_adapter" -o "$OUTPUT_PATH" "$@" "$EXPANDED_SOURCE_PATH"
-ccache g++-12 -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/stream_adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
+ccache g++-12 -std=gnu++17 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/stream_adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
