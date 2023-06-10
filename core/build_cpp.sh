dd77bcbf888ef135576e5494ed11f2c038c48192
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-06-10 17:46:51 +0900

    Update: command: cpp builder

--- core/build_cpp.sh
+++ core/build_cpp.sh
@@ -12,5 +12,6 @@ shift 3
 
 # ./expand_cpp.sh "$LIBRARY_PATH" "$TARGET" "$EXPANDED_SOURCE_PATH"
 # ccache g++ -std=gnu++17 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/adapter" -o "$OUTPUT_PATH" "$@" "$EXPANDED_SOURCE_PATH"
-ccache g++-12 -std=gnu++17 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -O2 -I"$HOME/boost" -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
+# -Wshadow
+ccache g++-12 -std=gnu++17 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -Wconversion -Wfloat-equal -Wno-char-subscripts -ftrapv -fstack-protector-all -g3 -O2 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -I"$HOME/boost" -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
 # g++-12 -S -std=gnu++17 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
