8184f08df08c198faf2357d1b724fd6662ac1d12
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-11-03 22:21:11 +0900

    fix-path

--- core/build_cpp.sh
+++ core/build_cpp.sh
@@ -11,7 +11,7 @@ OUTPUT_PATH=$3
 shift 3
 
 # ./expand_cpp.sh "$LIBRARY_PATH" "$TARGET" "$EXPANDED_SOURCE_PATH"
-# ccache g++ -std=gnu++20 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/src/debugger" -I"$LIBRARY_PATH/original/src/adapter" -o "$OUTPUT_PATH" "$@" "$EXPANDED_SOURCE_PATH"
+# ccache g++ -std=gnu++20 -fdiagnostics-color=always -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/adapter" -o "$OUTPUT_PATH" "$@" "$EXPANDED_SOURCE_PATH"
 # -Wshadow
-ccache g++-12 -std=gnu++20 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -fconcepts-diagnostics-depth=5 -Wno-char-subscripts -fsplit-stack -I"$HOME/boost" -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/src" -I"$LIBRARY_PATH/original/src/debugger" -o "$OUTPUT_PATH" "$@" "$TARGET"
-# g++-12 -S -std=gnu++20 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original/src" -I"$LIBRARY_PATH/original/src/debugger" -I"$LIBRARY_PATH/original/src/adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
+ccache g++-12 -std=gnu++20 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -fconcepts-diagnostics-depth=5 -Wno-char-subscripts -fsplit-stack -I"$HOME/boost" -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -o "$OUTPUT_PATH" "$@" "$TARGET"
+# g++-12 -S -std=gnu++20 -fdiagnostics-color=always -Wno-misleading-indentation -Wall -Wextra -O2 -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I"$LIBRARY_PATH/ac-library" -I"$LIBRARY_PATH/original" -I"$LIBRARY_PATH/original/debugger" -I"$LIBRARY_PATH/original/adapter" -o "$OUTPUT_PATH" "$@" "$TARGET"
