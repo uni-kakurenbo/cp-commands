07a4d4fdf16b7dcb682bd5a080b662be4d2d150a
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-11-05 10:09:25 +0900

    modify: pre-compile

--- pre-compile.sh
+++ pre-compile.sh
@@ -12,5 +12,5 @@ LIBRARY_PATH="$ROOT/sources/libraries"
 sudo rm "$BITS/stdc++.h.gch"
 
 
-# sudo "$ROOT/commands/ccore.sh" build_cpp "$LIBRARY_PATH" "$BITS/stdc++.h" "$BITS/stdc++.h.gch"
+sudo "$ROOT/commands/ccore.sh" build_cpp "$LIBRARY_PATH" "$BITS/stdc++.h" "$BITS/stdc++.h.gch"
 # sudo "$ROOT/commands/ccore.sh" build_cpp "$LIBRARY_PATH" "$LIBRARY_PATH/original/debugger/debug.hpp" "$LIBRARY_PATH/original/debugger/debug.hpp.gch"
